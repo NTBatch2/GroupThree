ac0976c0f089abaa58742e327e251185515d1660
mercy <mercy@DESKTOP-GT9KGLF>
2020-05-17 09:00:03 -0400

    when life is smile

--- src/noorteck/Jawed.java
+++ src/noorteck/Jawed.java
@@ -3,6 +3,7 @@ package noorteck;
 public class Jawed {
 	public static void main(String[] args) {
 		System.out.println("Practice More");
+		System.out.println("when life give u a lemon make a lemonade");
 	}
-
+   // when life give u a lemon make a lemonade
 }
