dd5dfed573a64d2e7a846f5ac0ad5b47105b22f6
mercy <mercy@DESKTOP-GT9KGLF>
2020-05-17 12:08:05 -0400

    summer is here

--- src/noorteck/Wawa.java
+++ src/noorteck/Wawa.java
@@ -3,6 +3,7 @@ package noorteck;
 public final class Wawa {
 	public static void main(String[] args) {
 		System.out.println("i need to practice more");
+		System.out.println("craving  spicy food");
 	}
 
 }
