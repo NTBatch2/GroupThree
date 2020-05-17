6195485a86d08449b4a0fbc60e234564d2c4551c
luwamhall <luwamhall@192.168.1.3>
2020-05-17 11:35:26 -0400

    05/17/2020 added code on the demo class

--- src/noorteck/Demo.java
+++ src/noorteck/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Group Three");
 		System.out.println("Practice more");
-	    System.out.println("Good morning team");
+	    System.out.println("Good sunday ");
 	
 	}
 	//mohammed code
